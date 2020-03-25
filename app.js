576dd7936440657238fec742ce1ebd21f917c8fa
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2020-03-25 21:29:34 +0100

    code refactoring  - app conversion to book api app

--- app.js
+++ app.js
@@ -2,33 +2,23 @@ const express = require('express');
 const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
-const path = require('path');
 
 const app = express();
 
 app.use(morgan('tiny'));
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')));
-app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')));
-app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist/')));
-
-app.set('views', './src/views');
-app.set('view engine', 'ejs');
 
 const port = process.env.PORT || 3000;
-const navs = [
-    { link: '/books', title: 'BOOKS' },
-    { link: '/authors', title: 'AUTHORS' },
-];
+const bookRouter = express.Router();
 
-const bookRouter = require('./src/routes/bookRoutes')(navs);
-
-app.use('/books', bookRouter);
-app.get('/', (req, res) => {
-    res.render('index', {
-        title: 'book library',
-        navs,
+// for book session
+bookRouter.route('/books')
+    .get((req, res) => {
+        res.json({ name: 'Paul Silanka' });
     });
+
+app.use('/api', bookRouter);
+app.get('/', (req, res) => {
+    res.json({});
 });
 
 app.listen(port, () => {
---
+++ bookJson.js
@@ -0,0 +1,32 @@
+db.books.insert([
+    {
+        title: 'things fall apart',
+        author: 'Chinua Achebe',
+        genre: 'african literature',
+        read: false
+    },
+    {
+        title: 'Purple Hibiscus',
+        author: 'Chimamanda Adichie',
+        genre: 'african literature',
+        read: false
+    },
+    {
+        title: 'Riches man in babylon',
+        author: 'George Clason',
+        genre: 'business',
+        read: false
+    },
+    {
+        title: 'There was a country',
+        author: 'Chinua Achebe',
+        genre: 'african literature',
+        read: false
+    },
+    {
+        title: 'Rich dad poor dad',
+        author: 'Robert Kiyosaki',
+        genre: 'busines',
+        read: false
+    }
+]);
